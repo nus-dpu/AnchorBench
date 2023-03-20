40211670c84883f1f7c5a315592fb5cb7771d82f
yihan <yangyihan1998@gmail.com>
2023-03-20 10:27:32 +0000

    dbg: debug dns-filter

--- dns-filter/dns-filter.c
+++ dns-filter/dns-filter.c
@@ -132,14 +132,12 @@ static int extract_dns_query(struct rte_mbuf *pkt) {
 
 	for(int i = 0 ; i < (int)strlen(p); i++)  {
 		printf("Check %x\n", p[i]);
-		if(isdigit(p[i])) {
-			printf("Is digit!\n");
-			len = p[i];
-			for (int j = 0; j < len; j++) {
-				*dst++ = p[++i];
-			}
-			*dst++ = '.';
+		len = p[i++];
+		for (int j = 0; j < len; j++) {
+			printf("==> %x\n", p[i]);
+			*dst++ = p[i++];
 		}
+		*dst++ = '.';
 	}
 
 	printf("Query for: %s\n", name);
