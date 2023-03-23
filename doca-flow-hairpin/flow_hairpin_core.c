2053550c18d07ddeb3bbfda95eb2ad2c7420ed5d
yihan <yangyihan1998@gmail.com>
2023-03-23 04:52:15 +0000

    feat: test raw hairpin

--- doca-flow-hairpin/flow_hairpin_core.c
+++ doca-flow-hairpin/flow_hairpin_core.c
@@ -91,10 +91,9 @@ add_hairpin_pipe_entry(struct doca_flow_pipe *pipe, struct doca_flow_port *port,
 	int num_of_entries = 1;
 
 	/* example 5-tuple to forward */
-	doca_be32_t dst_ip_addr = BE_IPV4_ADDR(8, 8, 8, 8);
-	doca_be32_t src_ip_addr = BE_IPV4_ADDR(1, 2, 3, 4);
-	doca_be16_t dst_port = rte_cpu_to_be_16(80);
-	doca_be16_t src_port = rte_cpu_to_be_16(1234);
+	doca_be32_t dst_ip_addr = BE_IPV4_ADDR(10, 0, 0, 1);
+	doca_be32_t src_ip_addr = BE_IPV4_ADDR(10, 0, 0, 3);
+	doca_be16_t dst_port = rte_cpu_to_be_16(1234);
 
 	memset(&match, 0, sizeof(match));
 	memset(&actions, 0, sizeof(actions));
@@ -102,7 +101,6 @@ add_hairpin_pipe_entry(struct doca_flow_pipe *pipe, struct doca_flow_port *port,
 	match.out_dst_ip.ipv4_addr = dst_ip_addr;
 	match.out_src_ip.ipv4_addr = src_ip_addr;
 	match.out_dst_port = dst_port;
-	match.out_src_port = src_port;
 
 	entry = doca_flow_pipe_add_entry(0, pipe, &match, &actions, NULL, NULL, 0, NULL, error);
 	result = doca_flow_entries_process(port, 0, DEFAULT_TIMEOUT_US, num_of_entries);
@@ -160,7 +158,7 @@ flow_hairpin(int nb_queues)
 	}
 
 	DOCA_LOG_INFO("Wait few seconds for packets to arrive");
-	sleep(5);
+	while (true);
 
 	destroy_doca_flow_ports(nb_ports, ports);
 	doca_flow_destroy();
