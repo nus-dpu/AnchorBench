69f5b4598e452ec4b9f44944d4e0291316733fce
yihan <yangyihan1998@gmail.com>
2023-03-23 05:32:04 +0000

    feat: offload hairpin

--- doca-flow-hairpin/flow_hairpin.c
+++ doca-flow-hairpin/flow_hairpin.c
@@ -34,6 +34,7 @@ int flow_hairpin(int nb_queues);
 int
 main(int argc, char **argv)
 {
+#if 0
 	doca_error_t result;
 	int ret;
 	int exit_status = EXIT_SUCCESS;
@@ -85,4 +86,51 @@ main(int argc, char **argv)
 	/* ARGP cleanup */
 	doca_argp_destroy();
 	return exit_status;
+#endif
+	int ret;
+	doca_error_t result;
+	int exit_status = EXIT_SUCCESS;
+	pthread_t cmdline_thread;
+	struct dpi_worker_attr dpi_worker = {0};
+	struct application_dpdk_config dpdk_config = {
+		.port_config.nb_ports = 2,
+		.port_config.nb_queues = 2,
+		.port_config.nb_hairpin_q = 4,
+		.sft_config = {
+			.enable = 1, /* Enable SFT */
+			.enable_ct = 1,
+			.enable_state_hairpin = 1,
+			.enable_state_drop = 1,
+		},
+		.reserve_main_thread = true,
+	};
+	struct ar_config ar_config = {.dpdk_config = &dpdk_config};
+
+	/* Parse cmdline/json arguments */
+	result = doca_argp_init("application_recognition", &ar_config);
+	if (result != DOCA_SUCCESS) {
+		DOCA_LOG_ERR("Failed to init ARGP resources: %s", doca_get_error_string(result));
+		return EXIT_FAILURE;
+	}
+	doca_argp_set_dpdk_program(dpdk_init);
+	result = register_ar_params();
+	if (result != DOCA_SUCCESS) {
+		DOCA_LOG_ERR("Failed to init ARGP resources: %s", doca_get_error_string(result));
+		doca_argp_destroy();
+		return EXIT_FAILURE;
+	}
+	result = doca_argp_start(argc, argv);
+	if (result != DOCA_SUCCESS) {
+		DOCA_LOG_ERR("Failed to parse application input: %s", doca_get_error_string(result));
+		doca_argp_destroy();
+		return EXIT_FAILURE;
+	}
+
+	/* update queues and ports */
+	result = dpdk_queues_and_ports_init(&dpdk_config);
+	if (result != DOCA_SUCCESS) {
+		DOCA_LOG_ERR("Failed to update application ports and queues: %s", doca_get_error_string(result));
+		exit_status = EXIT_FAILURE;
+		goto dpdk_destroy;
+	}
 }
