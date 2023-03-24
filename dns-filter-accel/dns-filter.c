4aa1be6e5d1cc3e0cefa8e26f72727e2f07275dd
yihan <yangyihan1998@gmail.com>
2023-03-24 09:21:34 +0000

    dbg: extract mmap populate

--- dns-filter-accel/dns-filter.c
+++ dns-filter-accel/dns-filter.c
@@ -402,8 +402,10 @@ dns_worker_lcores_run(struct dns_filter_config *app_cfg)
 				DOCA_LOG_ERR("Unable to acquire DOCA buffer for job data: %s", doca_get_error_string(result));
 				goto queries_cleanup;
 			}
+			void *mbuf_data;
+			doca_buf_get_data(worker_ctx->buf[i], &mbuf_data);
 
-			printf("worker_ctx buf[%d]: %p\n", i, worker_ctx->buf[i]);
+			printf("worker_ctx buf[%d]: %p, mbuf data: %p\n", i, worker_ctx->buf[i], mbuf_data);
 		}
 
 		/* Launch the worker to start process packets */
