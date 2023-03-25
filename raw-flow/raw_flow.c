3081db8e132b13847f06f64ea721a5f1caf6439c
yihan <yangyihan1998@gmail.com>
2023-03-25 01:57:00 +0000

    dbg: timestamp

--- raw-flow/raw_flow.c
+++ raw-flow/raw_flow.c
@@ -7,6 +7,62 @@
 #define MAX_PATTERN_NUM		4
 #define MAX_ACTION_NUM		2
 
+/* Configure the Rx and Tx hairpin queues for the selected port. */
+static int
+setup_hairpin_queues(uint16_t pi) {
+	uint16_t qi;
+	struct rte_eth_hairpin_conf hairpin_conf = {
+		.peer_count = 1,
+	};
+	int i;
+	int diag;
+	struct rte_port *port = &ports[pi];
+
+	for (qi = nb_txq, i = 0; qi < nb_hairpinq + nb_txq; qi++) {
+		hairpin_conf.peers[0].port = pi;
+		hairpin_conf.peers[0].queue = i + nb_rxq;
+		diag = rte_eth_tx_hairpin_queue_setup
+			(pi, qi, nb_txd, &hairpin_conf);
+		i++;
+		if (diag == 0)
+			continue;
+
+		/* Fail to setup rx queue, return */
+		if (rte_atomic16_cmpset(&(port->port_status),
+					RTE_PORT_HANDLING,
+					RTE_PORT_STOPPED) == 0)
+			printf("Port %d can not be set back "
+					"to stopped\n", pi);
+		printf("Fail to configure port %d hairpin "
+				"queues\n", pi);
+		/* try to reconfigure queues next time */
+		port->need_reconfig_queues = 1;
+		return -1;
+	}
+	for (qi = nb_rxq, i = 0; qi < nb_hairpinq + nb_rxq; qi++) {
+		hairpin_conf.peers[0].port = pi;
+		hairpin_conf.peers[0].queue = i + nb_txq;
+		diag = rte_eth_rx_hairpin_queue_setup
+			(pi, qi, nb_rxd, &hairpin_conf);
+		i++;
+		if (diag == 0)
+			continue;
+
+		/* Fail to setup rx queue, return */
+		if (rte_atomic16_cmpset(&(port->port_status),
+					RTE_PORT_HANDLING,
+					RTE_PORT_STOPPED) == 0)
+			printf("Port %d can not be set back "
+					"to stopped\n", pi);
+		printf("Fail to configure port %d hairpin "
+				"queues\n", pi);
+		/* try to reconfigure queues next time */
+		port->need_reconfig_queues = 1;
+		return -1;
+	}
+	return 0;
+}
+
 int main(int argc, char **argv) {
 	uint32_t i;
 	int32_t ret;
@@ -22,12 +78,16 @@ int main(int argc, char **argv) {
 	argv += ret;
 
     RTE_ETH_FOREACH_DEV(port_id) {
+        if (setup_hairpin_queues(port_id) != 0) {
+				return -1;
+        }
+
         struct rte_flow_error error;
         struct rte_flow_attr attr;
         struct rte_flow_item pattern[MAX_PATTERN_NUM];
         struct rte_flow_action action[MAX_ACTION_NUM];
         struct rte_flow * flow = NULL;
-        struct rte_flow_action_port_id port = { .id = port_id ^ 1 };
+        struct rte_flow_action_queue_id port = { .queue = port_id ^ 1 };
         struct rte_flow_item_ipv4 ip_spec;
         struct rte_flow_item_ipv4 ip_mask;
         struct rte_flow_item_udp udp_spec;
@@ -48,7 +108,7 @@ int main(int argc, char **argv) {
         * create the action sequence.
         * one action only,  move packet to queue
         */
-        action[0].type = RTE_FLOW_ACTION_TYPE_PORT_ID;
+        action[0].type = RTE_FLOW_ACTION_TYPE_QUEUE_ID;
         action[0].conf = &port;
         action[1].type = RTE_FLOW_ACTION_TYPE_END;
 
