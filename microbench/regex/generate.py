9bd550be648b5d61f70ec95bb3e64d1b8c9cde63
yihan <yangyihan1998@gmail.com>
2023-04-07 08:00:55 +0000

    fea: add file generator

--- microbench/regex/generate.py
+++ microbench/regex/generate.py
@@ -1,32 +1,32 @@
 import random
 
 psychology_dns = [
-    'www.americanpsychologicalassociation.org'
-    'www.psychologytoday.com'
-    'www.associationforpsychologicalscience.org'
-    'www.societyforindustrialpsychology.org'
-    'www.psychology.org.au'
-    'www.psychologyboard.gov.au'
-    'www.academyofpsychologicalclinicalscience.org'
-    'www.internationalneuropsychologicalsociety.org'
-    'www.psychologicalscience.org'
-    'www.psychotherapy.net'
-    'www.psychologysalon.com'
-    'www.psychotherapynetworker.org'
-    'www.psychologytools.com'
-    'www.psychologyconsultants.com.au'
-    'www.lifespanpsychology.com'
-    'www.psychologen-culemborg.nl'
-    'www.psychologicalscience.nl'
-    'www.psychologytoday.ru'
-    'www.alabamapsychology.org'
-    'www.psychology.wisc.edu'
-    'www.psychotherapy.com'
-    'www.psychologywa.org.au'
-    'www.psychology-aktuell.com'
-    'www.psychologyinc.com'
-    'www.salfordpsychology.org'
-    'www.psychologyofeating.com'
+    'www.americanpsychologicalassociation.org',
+    'www.psychologytoday.com',
+    'www.associationforpsychologicalscience.org',
+    'www.societyforindustrialpsychology.org',
+    'www.psychology.org.au',
+    'www.psychologyboard.gov.au',
+    'www.academyofpsychologicalclinicalscience.org',
+    'www.internationalneuropsychologicalsociety.org',
+    'www.psychologicalscience.org',
+    'www.psychotherapy.net',
+    'www.psychologysalon.com',
+    'www.psychotherapynetworker.org',
+    'www.psychologytools.com',
+    'www.psychologyconsultants.com.au',
+    'www.lifespanpsychology.com',
+    'www.psychologen-culemborg.nl',
+    'www.psychologicalscience.nl',
+    'www.psychologytoday.ru',
+    'www.alabamapsychology.org',
+    'www.psychology.wisc.edu',
+    'www.psychotherapy.com',
+    'www.psychologywa.org.au',
+    'www.psychology-aktuell.com',
+    'www.psychologyinc.com',
+    'www.salfordpsychology.org',
+    'www.psychologyofeating.com',
     'www.psychologycouncil.org.nz'
 ]
 
