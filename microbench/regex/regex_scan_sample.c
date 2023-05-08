2e5b89c3d2db9b42d887cccb6ca9a1eb3548ada4
yihan <yangyihan1998@gmail.com>
2023-05-08 07:53:54 +0000

    dbg: rename

--- microbench/regex/regex_scan_sample.c
+++ microbench/regex/regex_scan_sample.c
@@ -485,8 +485,8 @@ regex_scan(char *data_buffer, size_t data_buffer_len, struct doca_pci_bdf *pci_a
 	while (1) {
 		current_time = CurrentTime_nanoseconds();
 		if (current_time - start > 10 * NSEC_PER_SEC) {
-			printf("Enqueue: %lu, %6.2ld(RPS)\n", nb_enqueued, nb_enqueued * 1000000000 / (current_time - start));
-			printf("Dequeue: %lu, %6.2ld(RPS)\n", nb_dequeued, nb_dequeued * 1000000000 / (current_time - start));
+			printf("Enqueue: %u, %6.2ld(RPS)\n", nb_enqueued, nb_enqueued * 1000000000 / (current_time - start));
+			printf("Dequeue: %u, %6.2ld(RPS)\n", nb_dequeued, nb_dequeued * 1000000000 / (current_time - start));
 			break;
 		}
 
