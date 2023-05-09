2c1f1c7c4a617a8f358e494ca458d0eb3b758bf3
yihan <yangyihan1998@gmail.com>
2023-05-09 06:11:40 +0000

    dbg: debug workq

--- microbench/regex-2/regex.c
+++ microbench/regex-2/regex.c
@@ -8,9 +8,7 @@ int regex_work_lcore(void * arg) {
 	doca_error_t result;
 	struct regex_ctx * rgx_ctx = (struct regex_ctx *)arg;
 
-    regex_init_lcore(rgx_ctx);
-
-    printf("CPU %02d| initialization done!\n", sched_getcpu());
+    printf("CPU %02d| Work start!\n", sched_getcpu());
 
     return 0;
 }
