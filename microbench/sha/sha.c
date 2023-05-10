181eea1eb47c78752175980c5207ba6b366cfe3c
yihan <yangyihan1998@gmail.com>
2023-05-10 04:26:53 +0000

    feat: add sha microbench

--- microbench/sha/sha.c
+++ microbench/sha/sha.c
@@ -163,7 +163,7 @@ static int sha_deq_job(struct sha_ctx *ctx) {
 			doca_buf_refcount_rm(src_doca_buf->buf, NULL);
 			doca_buf_refcount_rm(dst_doca_buf->buf, NULL);
 			/* Put the element back into the mempool */
-			mempool_put(ctx->buf_mempool, buf_element);
+			mempool_put(ctx->buf_mempool, src_doca_buf);
 			mempool_put(ctx->buf_mempool, dst_doca_buf);
 			++finished;
 		} else if (result == DOCA_ERROR_AGAIN) {
