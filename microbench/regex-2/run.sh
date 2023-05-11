99ce09139d0b673c0833b582f0fe7f515e0a0300
yihan <yangyihan1998@gmail.com>
2023-05-11 07:21:25 +0000

    feat: add data size

--- microbench/regex-2/run.sh
+++ microbench/regex-2/run.sh
@@ -1,41 +1,25 @@
-mkdir long-url-result/
+data_size=(16 32 64 128 256 512 1024)
 
-for nr_core in $(seq 1 1 8); do 
-	echo ">> Test with $nr_core cores >>"
+for size in "${data_size[@]}"; do
+	mkdir ${sizeB}-result/
 
-	mkdir long-url-result/thp-$nr_core/
-	mkdir long-url-result/lat-$nr_core/
+	# for nr_core in $(seq 1 1 8); do 
+	# 	echo ">> Test with $nr_core cores >>"
 
-	for rate in $(seq 10 140 5000); do
-		rm thp-*.txt latency-*.txt
-		echo "  >> Test long URL input $rate (Kops)"
-		./build/regex -l 50 -p 03:00.0 -r /tmp/url_regex_rules.rof2.binary -d $(pwd)/long_url.txt -c $nr_core -s $rate
-		cat thp-*.txt > long-url-result/thp-$nr_core/thp-rate-$rate.txt
-		cat latency-*.txt > long-url-result/lat-$nr_core/lat-rate-$rate.txt
-		echo "  >> Test done!"
-		sleep 5
-	done
+	# 	mkdir long-url-result/thp-$nr_core/
+	# 	mkdir long-url-result/lat-$nr_core/
+
+	# 	for rate in $(seq 10 140 5000); do
+	# 		rm thp-*.txt latency-*.txt
+	# 		echo "  >> Test long URL input $rate (Kops)"
+	# 		./build/regex -l 50 -p 03:00.0 -r /tmp/url_regex_rules.rof2.binary -d $(pwd)/long_url.txt -c $nr_core -s $rate
+	# 		cat thp-*.txt > long-url-result/thp-$nr_core/thp-rate-$rate.txt
+	# 		cat latency-*.txt > long-url-result/lat-$nr_core/lat-rate-$rate.txt
+	# 		echo "  >> Test done!"
+	# 		sleep 5
+	# 	done
+	# done
+
+	# rm thp-*.txt latency-*.txt
+
 done
-
-rm thp-*.txt latency-*.txt
-
-mkdir short-url-result/
-
-for nr_core in $(seq 1 1 8); do 
-	echo ">> Test with $nr_core cores >>"
-
-	mkdir short-url-result/thp-$nr_core/
-	mkdir short-url-result/lat-$nr_core/
-
-	for rate in $(seq 10 140 5000); do
-		rm thp-*.txt latency-*.txt
-		echo "  >> Test short URL input $rate (Kops)"
-		./build/regex -l 50 -p 03:00.0 -r /tmp/url_regex_rules.rof2.binary -d $(pwd)/short_url.txt -c $nr_core -s $rate
-		cat thp-*.txt > short-urlresult/thp-$nr_core/thp-rate-$rate.txt
-		cat latency-*.txt > short-urlresult/lat-$nr_core/lat-rate-$rate.txt
-		echo "  >> Test done!"
-		sleep 5
-	done
-done
-
-rm thp-*.txt latency-*.txt
