4eda0313b3b494f1bff5e16c95832e206ab5bb0c
yihan <yangyihan1998@gmail.com>
2023-05-11 07:24:05 +0000

    feat: add data size

--- microbench/regex-2/run.sh
+++ microbench/regex-2/run.sh
@@ -3,23 +3,23 @@ data_size=(16 32 64 128 256 512 1024)
 for size in "${data_size[@]}"; do
 	mkdir ${size}B-result/
 
-	# for nr_core in $(seq 1 1 8); do 
-	# 	echo ">> Test with $nr_core cores >>"
+	for nr_core in $(seq 1 1 8); do 
+		echo ">> Test with $nr_core cores >>"
 
-	# 	mkdir long-url-result/thp-$nr_core/
-	# 	mkdir long-url-result/lat-$nr_core/
+		mkdir ${size}B-result/thp-$nr_core/
+		mkdir ${size}B-result/lat-$nr_core/
 
-	# 	for rate in $(seq 10 140 5000); do
-	# 		rm thp-*.txt latency-*.txt
-	# 		echo "  >> Test long URL input $rate (Kops)"
-	# 		./build/regex -l 50 -p 03:00.0 -r /tmp/url_regex_rules.rof2.binary -d $(pwd)/long_url.txt -c $nr_core -s $rate
-	# 		cat thp-*.txt > long-url-result/thp-$nr_core/thp-rate-$rate.txt
-	# 		cat latency-*.txt > long-url-result/lat-$nr_core/lat-rate-$rate.txt
-	# 		echo "  >> Test done!"
-	# 		sleep 5
-	# 	done
-	# done
+		for rate in $(seq 10 140 5000); do
+			rm thp-*.txt latency-*.txt
+			echo "  >> Test input $rate (Kops) with data size $size Bytes"
+			./build/regex -l 50 -p 03:00.0 -r /tmp/regex_rules.rof2.binary -d $(pwd)/alice_in_wonderland.txt -c $nr_core -s $rate
+			cat thp-*.txt > ${size}B-result/thp-$nr_core/thp-rate-$rate.txt
+			cat latency-*.txt > ${size}B-result/lat-$nr_core/lat-rate-$rate.txt
+			echo "  >> Test done!"
+			sleep 5
+		done
+	done
 
-	# rm thp-*.txt latency-*.txt
+	rm thp-*.txt latency-*.txt
 
 done
