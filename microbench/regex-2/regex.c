1d7f0a6f92811df487557b142664dbcd97b6b9b5
yihan <yangyihan1998@gmail.com>
2023-05-12 16:12:16 +0000

    dbg: remove print

--- microbench/regex-2/regex.c
+++ microbench/regex-2/regex.c
@@ -221,7 +221,6 @@ void * regex_work_lcore(void * arg) {
 		}
 		memcpy(input[nr_rule].line, line, read);
 		input[nr_rule].len = read;
-		printf("line: %s(%d), nread: %d\n", line, strlen(line), read);
 		nr_rule++;
 	}
 
@@ -259,7 +258,6 @@ void * regex_work_lcore(void * arg) {
 
 		for (int i = 0; i < WORKQ_DEPTH; i++) {
 			if (diff_timespec(&worker[i].last_enq_time, &current_time) > worker[i].interval) {
-				printf("line: %s, len: %d\n", input[index].line, input[index].len);
 				ret = regex_scan_enq_job(rgx_ctx, input[index].line, input[index].len);
 				if (ret < 0) {
 					DOCA_LOG_ERR("Failed to enqueue jobs");
