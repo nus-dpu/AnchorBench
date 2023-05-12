9a6d97b46ab480e21e1b0691b74ae9255676c8df
yihan <yangyihan1998@gmail.com>
2023-05-12 15:18:36 +0000

    dbg: regex rule generation

--- microbench/regex-2/generate.py
+++ microbench/regex-2/generate.py
@@ -2,10 +2,9 @@ import random
 import sys
 
 src_file = open(sys.argv[1], 'r')
-data = src_file.read()
+data = src_file.readlines()
 print(data)
 with open('url.txt', 'w') as f:
     for i in range(0, 1000):
         url = random.choice(data)
         f.write(url)
-        f.write('\n')
