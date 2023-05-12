7a960c272ac66ed785d68895af758f78f453facd
yihan <yangyihan1998@gmail.com>
2023-05-12 16:10:08 +0000

    dbg: regex rule generation

--- microbench/regex-2/generate.py
+++ microbench/regex-2/generate.py
@@ -3,6 +3,7 @@ import sys
 
 src_file = open(sys.argv[1], 'r')
 data = src_file.readlines()
+data = [x for x in data[:-1]]
 print(data)
 with open('url.txt', 'w') as f:
     for i in range(0, 1000):
