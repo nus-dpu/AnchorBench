24ba1ee1ef248a90ea9187d469f03c13ce28bd9d
yihan <yangyihan1998@gmail.com>
2023-05-12 15:21:48 +0000

    dbg: regex rule generation

--- microbench/regex-2/generate.py
+++ microbench/regex-2/generate.py
@@ -5,6 +5,6 @@ src_file = open(sys.argv[1], 'r')
 data = src_file.readlines()
 print(data)
 with open('url.txt', 'w') as f:
-    for i in range(0, 1000):
+    for i in range(0, 1200):
         url = random.choice(data)
         f.write(url)
