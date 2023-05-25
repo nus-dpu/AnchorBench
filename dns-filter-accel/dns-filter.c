d1da47695f7a2cfa4512d5ebe34b9066d7e8f4f4
yihan <yangyihan1998@gmail.com>
2023-05-25 16:00:39 +0000

    test: no dynamic buf inventory

--- dns-filter-accel/dns-filter.c
+++ dns-filter-accel/dns-filter.c
@@ -461,10 +461,10 @@ static doca_error_t dns_filter_init_lcore(struct dns_worker_ctx * ctx) {
 	struct mempool_elt *elt;
     list_for_each_entry(elt, &ctx->buf_mempool->elt_free_list, list) {
 		/* Create a DOCA buffer  for this memory region */
-		result = doca_buf_inventory_buf_by_addr(ctx->buf_inv, ctx->mmap, data_buf, MEMPOOL_BUF_SIZE, &elt->buf);
+		result = doca_buf_inventory_buf_by_addr(ctx->buf_inv, ctx->mmap, buf_element->addr, MEMPOOL_BUF_SIZE, &elt->buf);
 		if (result != DOCA_SUCCESS) {
 			DOCA_LOG_ERR("Failed to allocate DOCA buf");
-			return nb_enqueued;
+			exit(1);
 		}
 
 		elt->response = (void *)calloc(1, sizeof(struct doca_regex_search_result));
