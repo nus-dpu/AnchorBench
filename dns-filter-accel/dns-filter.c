34bac3dff32834b8c7b9cd074d7fc1475d14179c
yihan <yangyihan1998@gmail.com>
2023-05-29 15:01:33 +0000

    dbg

--- dns-filter-accel/dns-filter.c
+++ dns-filter-accel/dns-filter.c
@@ -442,14 +442,14 @@ static doca_error_t dns_filter_init_lcore(struct dns_worker_ctx * ctx) {
 		DOCA_LOG_ERR("Unable to add memory region to memory map. Reason: %s", doca_get_error_string(result));
 		return result;
 	}
-#if 0
+
 	/* Segment the region into pieces */
 	struct mempool_elt *elt;
     list_for_each_entry(elt, &ctx->buf_mempool->elt_free_list, list) {
 		elt->response = (void *)calloc(1, sizeof(struct doca_regex_search_result));
 		elt->packet = (char *)calloc(256, sizeof(char));
 	}
-#endif
+
 	return result;
 }
 
