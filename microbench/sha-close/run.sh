2efbfab7128a36b181974092dd554e2a61d91c68
yihan <yangyihan1998@gmail.com>
2023-05-31 07:27:10 +0000

    dbg

--- microbench/sha-close/run.sh
+++ microbench/sha-close/run.sh
@@ -1,4 +1,5 @@
 data_size=(16 32 64 128 256 512 1024)
+rate=5000
 
 for size in "${data_size[@]}"; do
 	mkdir ${size}B-result/
@@ -9,15 +10,14 @@ for size in "${data_size[@]}"; do
 		mkdir ${size}B-result/thp-$nr_core/
 		mkdir ${size}B-result/lat-$nr_core/
 
-		for rate in $(seq 10 140 5000); do
-			rm thp-*.txt latency-*.txt
-			echo "  >> Test input $rate (Kops)"
-			./build/sha -l 50 -p 03:00.0 -d $(pwd)/input.dat -c $nr_core -s $rate -b $size
-			cat thp-*.txt > ${size}B-result/thp-$nr_core/thp-rate-$rate.txt
-			cat latency-*.txt > ${size}B-result/lat-$nr_core/lat-rate-$rate.txt
-			echo "  >> Test done!"
-			sleep 5
-		done
+		rm thp-*.txt latency-*.txt
+		echo "  >> Test input..."
+		./build/sha -l 50 -p 03:00.0 -d $(pwd)/input.dat -c $nr_core -s $rate -q 128 -b $size
+		cat thp-*.txt > ${size}B-result/thp-$nr_core/thp-rate.txt
+		cat latency-*.txt > ${size}B-result/lat-$nr_core/lat-rate.txt
+		echo "  >> Test done!"
+		sleep 2
+
 	done
 
 	rm thp-*.txt latency-*.txt
