e89989cd8692daa081c98d2284bc3e59b8604ada
yihan <yangyihan1998@gmail.com>
2023-05-31 08:17:15 +0000

    dbg

--- microbench/sha-close/run.sh
+++ microbench/sha-close/run.sh
@@ -5,14 +5,14 @@ for size in "${data_size[@]}"; do
 	mkdir ${size}B-result/
 
 	for nr_core in $(seq 1 1 8); do 
-		echo ">> Test with $nr_core cores >>"
+		echo ">> Test with $nr_core cores and $size B data size >>"
 
 		mkdir ${size}B-result/thp-$nr_core/
 		mkdir ${size}B-result/lat-$nr_core/
 
 		for round in $(seq 1 1 10) do
 			rm thp-*.txt latency-*.txt
-			echo "  >> Test input..."
+			echo "  >> Test round $round..."
 			./build/sha -l 50 -p 03:00.0 -d $(pwd)/input.dat -c $nr_core -s $rate -q 128 -b $size
 			cat thp-*.txt > ${size}B-result/thp-$nr_core/thp-rate-$round.txt
 			cat latency-*.txt > ${size}B-result/lat-$nr_core/lat-rate-$round.txt
