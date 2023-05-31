7860b57bebd593a2801eb3da500955acfa525c18
yihan <yangyihan1998@gmail.com>
2023-05-31 03:19:44 +0000

    dbg

--- microbench/regex-close/run_dns.sh
+++ microbench/regex-close/run_dns.sh
@@ -1,26 +1,25 @@
 # queue_depth=(1 4 16 64 256)
 queue_depth=128
+rate=5000
 
-rm url.txt
+rm input.txt
 python generate.py dns_baseline.txt
 
-mkdir dns-result/
+mkdir dns-local-result/
 
 for nr_core in $(seq 1 1 8); do 
     echo ">> Test with $nr_core cores >>"
 
-    mkdir dns-result/thp-$nr_core/
-    mkdir dns-result/lat-$nr_core/
+    mkdir dns-local-result/thp-$nr_core/
+    mkdir dns-local-result/lat-$nr_core/
 
-    for rate in $(seq 10 80 4600); do
-        rm thp-*.txt latency-*.txt
-        echo "  >> Test input $rate (Kops) with data size $size Bytes"
-        ./build/regex -l 50 -p 03:00.0 -r /tmp/dns_baseline.rof2.binary -d $(pwd)/input.txt -c $nr_core -s $rate -q ${queue_depth}
-        cat thp-*.txt > dns-result/thp-$nr_core/thp-rate-$rate.txt
-        cat latency-*.txt > dns-result/lat-$nr_core/lat-rate-$rate.txt
-        echo "  >> Test done!"
-        sleep 2
-        rm thp-*.txt latency-*.txt
-    done
+    rm thp-*.txt latency-*.txt
+    echo "  >> Test input $rate (Kops) with data size $size Bytes"
+    ./build/regex -l 50 -p 03:00.0 -r /tmp/dns_baseline.rof2.binary -d $(pwd)/input.txt -c $nr_core -s $rate -q ${queue_depth}
+    cat thp-*.txt > dns-local-result/thp-$nr_core/thp-rate-$rate.txt
+    cat latency-*.txt > dns-local-result/lat-$nr_core/lat-rate-$rate.txt
+    echo "  >> Test done!"
+    sleep 2
+    rm thp-*.txt latency-*.txt
 
 done
