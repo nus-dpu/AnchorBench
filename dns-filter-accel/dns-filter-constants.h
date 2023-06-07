b248048ef930d827cd3d48e142d8b2f990100640
yihan <yangyihan1998@gmail.com>
2023-06-07 09:03:41 +0000

    remove dbg test

--- dns-filter-accel/dns-filter-constants.h
+++ dns-filter-accel/dns-filter-constants.h
@@ -17,8 +17,8 @@ enum {
 	MBUF_SIZE		= RTE_MBUF_DEFAULT_BUF_SIZE + DEFAULT_PRIV_SIZE, /* See: http://dpdk.org/dev/patchwork/patch/4479/ */
 
 	NUM_Q                   = 16,
-	MEMPOOL_NR_BUF			= 32,
- 	MEMPOOL_BUF_SIZE		= 1024,
+	MEMPOOL_NR_BUF			= 256,
+ 	MEMPOOL_BUF_SIZE		= 256,
 };
 
 #endif  /* _DNS_FILTER_CONSTANTS_H_ */
