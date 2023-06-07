a3013fc5b5c7cdaf570ab01ab29d2b9b498c6fde
yihan <yangyihan1998@gmail.com>
2023-06-07 07:24:22 +0000

    update

--- microbench/ram_bench/list_traversal.c
+++ microbench/ram_bench/list_traversal.c
@@ -80,7 +80,7 @@ int main(int argc, const char * argv[])
 
 	Int stopsPerFactor = 4; // For every power of 2, how many measurements do we do?
 	Int minElemensFactor = 6;  // First measurement is 2^this number of elements.
-	Int maxElemsFactor = 30; // Last measurement is 2^this number of elements. 30 == 16GB of memory
+	Int maxElemsFactor = 12; // Last measurement is 2^this number of elements. 30 == 16GB of memory
 	//Int elemsPerMeasure = Int(1) << 28; // measure enough times to process this many elements (to get a good average)
 
 	Int min = stopsPerFactor * minElemensFactor;
