815d5e617c2bd63328dc2a23fd3438e9c23dc6a1
yihan <yangyihan1998@gmail.com>
2023-06-08 03:15:11 +0000

    dbg

--- microbench/regex/run_batch-2.sh
+++ microbench/regex/run_batch-2.sh
@@ -10,19 +10,19 @@ echo ">> Partial matching..."
 
 rm thp-*.txt latency-*.txt
 
-dir=${data_size}B-partial-batch=${batch}-result
+dir=${data_size}B-full-batch=${batch}-result
 mkdir ${dir}
 
 for nr_core in $(seq 1 1 8); do 
-	echo ">> Partial matching | Test with $nr_core cores >>"
+	echo ">> Full matching | Test with $nr_core cores >>"
 
 	mkdir ${dir}/thp-$nr_core/
 	mkdir ${dir}/lat-$nr_core/
 
 	for round in $(seq 1 1 6); do
 		rm thp-*.txt latency-*.txt
-		echo "  >> Partial matching | Test $data_size B (round $round)"
-		./build/regex -l 50 -p 03:00.0 -r /tmp/partial_url_regex_rules.rof2.binary -d $(pwd)/input.txt -c $nr_core -s $rate -q ${queue_depth}
+		echo "  >> Full matching | Test $data_size B (round $round)"
+		./build/regex -l 50 -p 03:00.0 -r /tmp/full_url_regex_rules.rof2.binary -d $(pwd)/input.txt -c $nr_core -s $rate -q ${queue_depth}
 		cat thp-*.txt 		> ${dir}/thp-$nr_core/thp-rate-$rate.txt
 		cat latency-*.txt 	> ${dir}/lat-$nr_core/lat-rate-$rate.txt
 		echo "  >> Test done!"
