f836b8c4ab550da684d693f5523ad8bc4e719559
yihan <yangyihan1998@gmail.com>
2023-06-08 14:18:00 +0000

    update

--- microbench/compress/run_max.sh
+++ microbench/compress/run_max.sh
@@ -10,7 +10,7 @@ for size in "${data_size[@]}"; do
 		mkdir ${size}B-result/thp-$nr_core/
 		mkdir ${size}B-result/lat-$nr_core/
 
-		for round in $(seq 1 1 10); do
+		# for round in $(seq 1 1 10); do
 			rm thp-*.txt latency-*.txt
 			echo "  >> Test $data_size B (round $round)"
 			./build/compress -l 50 -p 03:00.0 -d $(pwd)/input.dat -c $nr_core -s $rate -b $size
@@ -18,7 +18,7 @@ for size in "${data_size[@]}"; do
 			cat latency-*.txt > ${size}B-result/lat-$nr_core/lat-rate-$round.txt
 			echo "  >> Test done!"
 			sleep 2
-		done
+		# done
 	done
 
 	rm thp-*.txt latency-*.txt
