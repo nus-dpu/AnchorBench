cb88559bbf3068d9e4b70d917d41f52542639b30
yihan <yangyihan1998@gmail.com>
2023-06-09 15:08:41 +0000

    dbg

--- microbench/regex/run_singleflush.sh
+++ microbench/regex/run_singleflush.sh
@@ -23,7 +23,7 @@ for size in "${data_size[@]}"; do
 			rm thp-*.txt latency-*.txt
 			echo "  >> Full matching | Test $size B (round $round)"
 			./build/regex -l 50 -p 03:00.0 -r /tmp/full_url_regex_rules.rof2.binary -d $(pwd)/input.txt -c $nr_core -s $rate -q ${queue_depth}
-			mv thp-*.txt 		${dir}/thp-$nr_core/thp-rate-$rate.txt
+			mv thp-*.txt 		${dir}/thp-$nr_core/
 			mv latency-*.txt 	${dir}/lat-$nr_core/
 			echo "  >> Test done!"
 			sleep 2
