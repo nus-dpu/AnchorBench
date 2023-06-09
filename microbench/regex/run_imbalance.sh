cee145db71cc15a5234450fc4beb7fc5c478eb1a
yihan <yangyihan1998@gmail.com>
2023-06-09 16:09:05 +0000

    update imbalance

--- microbench/regex/run_imbalance.sh
+++ microbench/regex/run_imbalance.sh
@@ -1,4 +1,4 @@
-queue_depth=1
+queue_depth=128
 data_size=(20 60 100 200 400)
 nr_core=6
 
@@ -14,19 +14,17 @@ for size in "${data_size[@]}"; do
 	mkdir ${dir}
 
 	for rate in $(seq 10 120 5000); do 
-		echo ">> Full matching | Test with $nr_core cores >>"
+		echo ">> Full matching | Test at rate $rate >>"
 
-		mkdir ${dir}/thp-$nr_core/
-		mkdir ${dir}/lat-$nr_core/
+		mkdir ${dir}/thp-$rate/
+		mkdir ${dir}/lat-$rate/
 
-		# for round in $(seq 1 1 6); do
-			rm thp-*.txt latency-*.txt
-			echo "  >> Full matching | Test $size B (round $round)"
-			./build/regex -l 50 -p 03:00.0 -r /tmp/full_url_regex_rules.rof2.binary -d $(pwd)/input.txt -c $nr_core -s $rate -q ${queue_depth}
-			mv thp-*.txt 		${dir}/thp-$nr_core/
-			mv latency-*.txt 	${dir}/lat-$nr_core/
-			echo "  >> Test done!"
-			sleep 2
-		# done
+        rm thp-*.txt latency-*.txt
+        echo "  >> Full matching | Test $size B"
+        ./build/regex -l 50 -p 03:00.0 -r /tmp/full_url_regex_rules.rof2.binary -d $(pwd)/input.txt -c $nr_core -s $rate -q ${queue_depth}
+        mv thp-*.txt 		${dir}/thp-$rate/
+        mv latency-*.txt 	${dir}/lat-$rate/
+        echo "  >> Test done!"
+        sleep 2
 	done
 done
