28996de4f2a180784fb61c2154d9b4e28c7d2d14
yihan <yangyihan1998@gmail.com>
2023-06-10 12:56:14 +0000

    need to test baseline

--- microbench/regex/regex_main.c
+++ microbench/regex/regex_main.c
@@ -363,16 +363,6 @@ static doca_error_t regex_init(struct regex_config *regex_cfg) {
 static doca_error_t regex_init_lcore(struct regex_ctx * ctx) {
     doca_error_t result;
 
-	if (sched_getcpu() < 2) {
-		// cfg.queue_depth = 1;
-		cfg.queue_depth = 32;
-	} else if (sched_getcpu() < 4) {
-		cfg.queue_depth = 8;
-	} else {
-		// cfg.queue_depth = 32;
-		cfg.queue_depth = 1;
-	}
-
     result = doca_workq_create(cfg.queue_depth, &ctx->workq);
 	if (result != DOCA_SUCCESS) {
 		DOCA_LOG_ERR("Unable to create work queue. Reason: %s", doca_get_error_string(result));
