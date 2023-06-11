ce5b7faa5f17e86178965819bfc837e9c8f62835
yihan <yangyihan1998@gmail.com>
2023-06-11 03:03:49 +0000

    dbg

--- microbench/regex-imbalance/regex.c
+++ microbench/regex-imbalance/regex.c
@@ -219,10 +219,10 @@ void * regex_work_lcore(void * arg) {
 	double mean = NUM_WORKER * cfg.nr_core * 1.0e6 / cfg.rate;
 	double max = NUM_WORKER * cfg.nr_core * 1.0e6 / 5000.00;
 	if (sched_getcpu() < 2) {
-		mean = mean / (1.6 * 1.6);
+		mean = mean / (1.4 * 1.4);
 		dec_start = 660;
 	} else if (sched_getcpu() < 4) {
-		mean = mean / 1.6;
+		mean = mean / 1.4;
 		dec_start = 580;
 	} else {
 		dec_start = 500;
