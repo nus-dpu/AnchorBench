ce6a8c711a143ce457e5b5f2279b31cf205af030
yihan <yangyihan1998@gmail.com>
2023-06-11 03:06:39 +0000

    dbg

--- microbench/regex-imbalance/regex.c
+++ microbench/regex-imbalance/regex.c
@@ -328,10 +328,10 @@ void * regex_work_lcore(void * arg) {
 
 		if (current_time.tv_sec - last_mean_change.tv_sec >= 8) {
 			if (increase_rate) {
-				mean -= 120000.00;
+				mean -= 80000.00;
 				printf("CPU %02d| Decrease >> new mean: %.2f\n", sched_getcpu(), mean);
 			} else {
-				mean += 120000.00;
+				mean += 80000.00;
 				printf("CPU %02d| Increase >> new mean: %.2f\n", sched_getcpu(), mean);
 			}
 
