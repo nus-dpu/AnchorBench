117bc1ac6c5ab6bcec7e9bd8c25d5630d433e146
yihan <yangyihan1998@gmail.com>
2023-06-11 02:55:11 +0000

    update

--- microbench/regex-imbalance/regex.c
+++ microbench/regex-imbalance/regex.c
@@ -215,12 +215,17 @@ void * regex_work_lcore(void * arg) {
 	int nr_thp_info = 0;
 	struct thp_info * thp_info = (struct thp_info *)calloc(240, sizeof(struct thp_info));
 
+	int dec_start;
 	double mean = NUM_WORKER * cfg.nr_core * 1.0e6 / cfg.rate;
 	double max = NUM_WORKER * cfg.nr_core * 1.0e6 / 5000.00;
 	if (sched_getcpu() < 2) {
 		mean = mean / 4;
+		dec_start = 660;
 	} else if (sched_getcpu() < 4) {
 		mean = mean / 2;
+		dec_start = 580;
+	} else {
+		dec_start = 500;
 	}
 
 	printf("CPU %02d| mean: %.2f, max: %.2f\n", sched_getcpu(), mean, max);
@@ -283,7 +288,7 @@ void * regex_work_lcore(void * arg) {
 
 	while (1) {
     	clock_gettime(CLOCK_MONOTONIC, &current_time);
-		if (current_time.tv_sec - begin.tv_sec > 220) {
+		if (current_time.tv_sec - begin.tv_sec > 1200) {
             clock_gettime(CLOCK_MONOTONIC, &end);
 			printf("CPU %02d| Enqueue: %u, %6.2lf(RPS), dequeue: %u, %6.2lf(RPS)\n", sched_getcpu(),
                 nb_enqueued, nb_enqueued * 1000000000.0 / (double)(TIMESPEC_TO_NSEC(end) - TIMESPEC_TO_NSEC(begin)),
@@ -317,18 +322,17 @@ void * regex_work_lcore(void * arg) {
             clock_gettime(CLOCK_MONOTONIC, &last_log);
 		}
 
+		if (current_time.tv_sec - begin.tv_sec > dec_start) {
+			increase_rate = false;
+		}
+
 		if (current_time.tv_sec - last_mean_change.tv_sec >= 8) {
 			if (increase_rate) {
 				mean -= 120000.00;
-				printf("CPU %02d| Decrease >> new mean: %.2f\n", sched_getcpu(), mean);
 			} else {
-				mean += 40000.00;
-				printf("CPU %02d| Increase >> new mean: %.2f\n", sched_getcpu(), mean);
+				mean += 120000.00;
 			}
-
-			if (mean < max) {
-				increase_rate = false;
-			}
+]
             clock_gettime(CLOCK_MONOTONIC, &last_mean_change);
 		}
 #if 0
