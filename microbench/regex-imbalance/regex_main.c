a8f81b3de0a63e6f8753a4ecd41db90f6b7ec6a5
yihan <yangyihan1998@gmail.com>
2023-06-12 08:30:11 +0000

    imbalance queue

--- microbench/regex-imbalance/regex_main.c
+++ microbench/regex-imbalance/regex_main.c
@@ -362,8 +362,15 @@ static doca_error_t regex_init(struct regex_config *regex_cfg) {
 
 static doca_error_t regex_init_lcore(struct regex_ctx * ctx) {
     doca_error_t result;
+	int depth = cfg.queue_depth;
 
-    result = doca_workq_create(cfg.queue_depth, &ctx->workq);
+	if (sched_getcpu() < 2) {
+		depth /= 4;
+	} else if (sched_getcpu() < 4) {
+		depth /= 2;
+	}
+
+    result = doca_workq_create(depth, &ctx->workq);
 	if (result != DOCA_SUCCESS) {
 		DOCA_LOG_ERR("Unable to create work queue. Reason: %s", doca_get_error_string(result));
 		// regex_scan_destroy(&rgx_cfg);
