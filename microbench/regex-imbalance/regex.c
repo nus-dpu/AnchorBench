e493d9d7731d2001d240afa35d85d109d2db7f35
yihan <yangyihan1998@gmail.com>
2023-06-13 16:39:39 +0000

    test with 8 cores

--- microbench/regex-imbalance/regex.c
+++ microbench/regex-imbalance/regex.c
@@ -226,19 +226,19 @@ void * regex_work_lcore(void * arg) {
 	double max = NUM_WORKER * cfg.nr_core * 1.0e6 / 5000.00;
 	double epoch = 0.0;
 	if (sched_getcpu() < 2) {
-		mean = mean / 8;
-		dec_start = 300;
-	 	lower_bound = 38000.0;
-		epoch = 19000;
-	} else if (sched_getcpu() < 5) {
 		mean = mean / 4;
+		dec_start = 300;
+	 	lower_bound = 40000.0;
+		epoch = 20000;
+	} else if (sched_getcpu() < 5) {
+		mean = mean / 2;
 		dec_start = 250;
 		lower_bound = 24000.0;
 		epoch = 24000;
 	} else {
 		dec_start = 200;
 		lower_bound = 0.0;
-		epoch = 20000;
+		epoch = 22000;
 	}
 
 	printf("CPU %02d| mean: %.2f, max: %.2f\n", sched_getcpu(), mean, max);
