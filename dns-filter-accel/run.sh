a10b1aa1197aca2e00e0f8600b6223acb445d776
yihan <yangyihan1998@gmail.com>
2023-06-17 11:42:25 +0000

    update run.sh

--- dns-filter-accel/run.sh
+++ dns-filter-accel/run.sh
@@ -3,7 +3,6 @@ start=$2
 step=$3
 max_rate=$4
 
-mkdir lat-$nr_core/
 mkdir thp-$nr_core/
 
 echo ">> Test with $nr_core cores >>"
@@ -12,7 +11,6 @@ for rate in $(seq $start $step $max_rate); do
     echo ">> Rate at $rate... >>"
     rm latency-*.txt thp-*.txt
     ./build/dns-filter -l 0-${nr_core} -n 4 -a 03:00.0 -a 03:00.1 -- -m "[1-$nr_core:-].0,[-:1-$nr_core].1" -q 128 -- -l 50 -r /tmp/dns_baseline.rof2.binary -p 03:00.0
-    cat latency-*.txt > lat-$nr_core/lat-rate-$rate.txt
     cat thp-*.txt > thp-$nr_core/thp-rate-$rate.txt
     echo "  >> Test done!"
     sleep 2
