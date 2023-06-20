e49f9e3cc8c46554da7d3242e334ce0bd1b63819
yihan <yangyihan1998@gmail.com>
2023-06-20 10:11:03 +0000

    Update compress mempool

--- microbench/compress/compress.c
+++ microbench/compress/compress.c
@@ -62,21 +62,19 @@ static int compress_enq_job(struct compress_ctx * ctx, char * data, int data_len
 	// doca_buf_inventory_get_num_free_elements(ctx->buf_inv, &nb_free);
 
 	// if (nb_free != 0) {
-		struct mempool_elt * src_buf, * dst_buf;
-		char * src_data_buf, * dst_data_buf;
+		struct mempool_elt * buf;
+		char * src_buf, * dst_buf;
 		void *mbuf_data;
 
 		/* Get one free element from the mempool */
-		mempool_get(ctx->buf_mempool, &src_buf);
-		// assert(src_buf != NULL);
-		mempool_get(ctx->buf_mempool, &dst_buf);
+		mempool_get(ctx->buf_mempool, &buf);
 		// assert(dst_buf != NULL);
 		/* Get the memory segment */
-		src_data_buf = src_buf->addr;
-		dst_data_buf = dst_buf->addr;
+		src_buf = buf->src_addr;
+		dst_buf = buf->src_addr;
 
 		// memset(src_data_buf, 0, BUF_SIZE);
-		memcpy(src_data_buf, data, data_len);
+		memcpy(src_buf, data, data_len);
 
 		// /* Create a DOCA buffer for this memory region */
 		// result = doca_buf_inventory_buf_by_addr(ctx->buf_inv, ctx->mmap, src_data_buf, BUF_SIZE, &src_buf->buf);
@@ -92,10 +90,8 @@ static int compress_enq_job(struct compress_ctx * ctx, char * data, int data_len
 		// 	return nb_enqueued;
 		// }
 
-		doca_buf_get_data(src_buf->buf, &mbuf_data);
-		doca_buf_set_data(src_buf->buf, mbuf_data, data_len);
-
-		src_buf->response = dst_buf;
+		doca_buf_get_data(buf->src_buf, &mbuf_data);
+		doca_buf_set_data(buf->src_buf, mbuf_data, data_len);
 
 	    clock_gettime(CLOCK_MONOTONIC, &src_buf->ts);
 
@@ -106,16 +102,15 @@ static int compress_enq_job(struct compress_ctx * ctx, char * data, int data_len
 				.ctx = doca_compress_as_ctx(ctx->doca_compress),
 				.user_data = { .ptr = src_buf },
 			},
-			.dst_buff = dst_buf->buf,
-			.src_buff = src_buf->buf,
+			.dst_buff = buf->dst_buf,
+			.src_buff = buf->src_buf,
 		};
 
 		result = doca_workq_submit(ctx->workq, (struct doca_job *)&compress_job);
 		if (result == DOCA_ERROR_NO_MEMORY) {
 			// doca_buf_refcount_rm(src_buf->buf, NULL);
 			// doca_buf_refcount_rm(dst_buf->buf, NULL);
-			mempool_put(ctx->buf_mempool, src_buf);
-			mempool_put(ctx->buf_mempool, dst_buf);
+			mempool_put(ctx->buf_mempool, buf);
 			return nb_enqueued; /* qp is full, try to dequeue. */
 		}
 		if (result != DOCA_SUCCESS) {
@@ -161,7 +156,7 @@ static int compress_deq_job(struct compress_ctx *ctx) {
 	struct timespec ts;
 	uint32_t nb_free = 0;
 	uint32_t nb_total = 0;
-	struct mempool_elt * src_doca_buf, * dst_doca_buf;
+	struct mempool_elt * buf;
 	struct timespec now;
 
 	clock_gettime(CLOCK_MONOTONIC, &now);
@@ -169,10 +164,9 @@ static int compress_deq_job(struct compress_ctx *ctx) {
 	do {
 		result = doca_workq_progress_retrieve(ctx->workq, &event, DOCA_WORKQ_RETRIEVE_FLAGS_NONE);
 		if (result == DOCA_SUCCESS) {
-			src_doca_buf = (struct mempool_elt *)event.user_data.ptr;
-			dst_doca_buf = (struct mempool_elt *)src_doca_buf->response;
+			buf = (struct mempool_elt *)event.user_data.ptr;
 			if (start_record && nr_latency < MAX_NR_LATENCY) {
-				latency[nr_latency++] = diff_timespec(&src_doca_buf->ts, &now);
+				latency[nr_latency++] = diff_timespec(&buf->ts, &now);
 			}
 			/* release the buffer back into the pool so it can be re-used */
 			// doca_buf_inventory_get_num_elements(ctx->buf_inv, &nb_total);
@@ -182,8 +176,7 @@ static int compress_deq_job(struct compress_ctx *ctx) {
 			// doca_buf_refcount_rm(src_doca_buf->buf, NULL);
 			// doca_buf_refcount_rm(dst_doca_buf->buf, NULL);
 			/* Put the element back into the mempool */
-			mempool_put(ctx->buf_mempool, src_doca_buf);
-			mempool_put(ctx->buf_mempool, dst_doca_buf);
+			mempool_put(ctx->buf_mempool, buf);
 			++finished;
 		} else if (result == DOCA_ERROR_AGAIN) {
 			break;
@@ -244,14 +237,16 @@ void * compress_work_lcore(void * arg) {
 
 	doca_error_t result;
 	struct mempool_elt *elt;
-	int index = 0;
-	void * res = (void *)calloc(NB_BUF, sizeof(uint64_t));
 
-    list_for_each_entry(elt, &compress_ctx->buf_mempool->elt_free_list, list) {
-		elt->response = &res[index++];
-	
+    list_for_each_entry(elt, &sha_ctx->buf_mempool->elt_free_list, list) {
 		/* Create a DOCA buffer for this memory region */
-		result = doca_buf_inventory_buf_by_addr(compress_ctx->buf_inv, compress_ctx->mmap, elt->addr, BUF_SIZE, &elt->buf);
+		result = doca_buf_inventory_buf_by_addr(sha_ctx->buf_inv, sha_ctx->mmap, elt->src_addr, BUF_SIZE, &elt->src_buf);
+		if (result != DOCA_SUCCESS) {
+			DOCA_LOG_ERR("Failed to allocate DOCA buf");
+		}
+
+		/* Create a DOCA buffer for this memory region */
+		result = doca_buf_inventory_buf_by_addr(sha_ctx->buf_inv, sha_ctx->mmap, elt->dst_addr, BUF_SIZE, &elt->dst_buf);
 		if (result != DOCA_SUCCESS) {
 			DOCA_LOG_ERR("Failed to allocate DOCA buf");
 		}
