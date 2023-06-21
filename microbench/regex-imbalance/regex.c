5d95c25b885cc64a28c22880b4597f75c873a097
yihan <yangyihan1998@gmail.com>
2023-06-21 03:34:55 +0000

    add api test

--- microbench/regex-imbalance/regex.c
+++ microbench/regex-imbalance/regex.c
@@ -97,7 +97,7 @@ static int regex_scan_enq_job(struct regex_ctx * ctx, char * data, int data_len)
 				.rule_group_ids = {1, 0, 0, 0},
 				.buffer = buf_element->buf,
 				.result = (struct doca_regex_search_result *)buf_element->response,
-				.allow_batching = false,
+				.allow_batching = true,
 				// .allow_batching = ((nb_enqueued + 1) % batch_size != 0),
 		};
 
