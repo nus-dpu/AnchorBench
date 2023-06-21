7998109e124dc22611242d80e946f7e5d71bc8e4
yihan <yangyihan1998@gmail.com>
2023-06-21 07:56:01 +0000

    dbg

--- microbench/regex-imbalance/run_imbalance.sh
+++ microbench/regex-imbalance/run_imbalance.sh
@@ -14,8 +14,8 @@ dir=${size}B-full-imbalance-result
 mkdir ${dir}
 mkdir ${dir}/thp/
 
-for nr_core in $(seq 0 1 7); do 
-    mkdir ${dir}/lat-${nr_core}/
+for i in $(seq 0 1 7); do 
+    mkdir ${dir}/lat-$i/
 done
 
 echo ">> Full matching | Test at rate $rate >>"
@@ -24,6 +24,6 @@ rm thp-*.txt latency-*.txt
 echo "  >> Full matching | Test $size B"
 ./build/regex -l 50 -p 03:00.0 -r /tmp/full_url_regex_rules.rof2.binary -d $(pwd)/input.txt -c $nr_core -s $rate -q ${queue_depth}
 mv thp-*.txt ${dir}/thp/
-for nr_core in $(seq 0 1 7); do 
-    mv latency-${nr_core}.txt   ${dir}/lat-${nr_core}/
+for i in $(seq 0 1 7); do 
+    mv latency-$i.txt   ${dir}/lat-$i/
 done
