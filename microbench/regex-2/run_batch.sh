f7251225780a99e88a7eb52d520ead47bbed407b
yihan <yangyihan1998@gmail.com>
2023-06-23 17:06:33 +0000

    test 2nd imbalance

--- microbench/regex-2/run_batch.sh
+++ microbench/regex-2/run_batch.sh
@@ -1,6 +1,6 @@
 queue_depth=128
 data_size=(20 60 100 200)
-batch_size=(2 4 8 16 32 64 128)
+batch_size=(2 4 8 16 32 64)
 per_core_rate=800
 
 for len in "${data_size[@]}"; do
@@ -12,7 +12,7 @@ for len in "${data_size[@]}"; do
 		dir=${len}B-full-batch=${size}-percore=${per_core_rate}-result
 		mkdir ${dir}
 		
-		for nr_core in $(seq 1 1 5); do 
+		for nr_core in $(seq 1 1 4); do 
 			echo ">> Full matching | Test with $nr_core cores >>"
 
 			rate=$((nr_core * per_core_rate))
@@ -23,8 +23,8 @@ for len in "${data_size[@]}"; do
 			rm thp-*.txt latency-*.txt
 			echo "  >> Full matching | Test $len B (round $round)"
 			./build/regex -l 50 -p 03:00.0 -r /tmp/full_url_regex_rules.rof2.binary -d $(pwd)/input.txt -c $nr_core -s $rate -q ${queue_depth} -a ${size}
-			cat thp-*.txt 		> ${dir}/thp-$nr_core/thp-rate-$rate.txt
-			cat latency-*.txt 	> ${dir}/lat-$nr_core/lat-rate-$rate.txt
+			mov thp-*.txt		${dir}/thp-$nr_core/
+			mov latency-*.txt 	${dir}/lat-$nr_core/
 			echo "  >> Test done!"
 			sleep 2
 		done
--- microbench/regex-2/run_rate.sh
+++ microbench/regex-2/run_rate.sh
@@ -1,28 +1,31 @@
 queue_depth=128
-data_size=(20 60 100 200)
-batch_size=(1 2 4 8 16 32 64 128)
+data_size=(20)
+batch_size=(1 2 4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64)
+# batch_size=inf
 
 for len in "${data_size[@]}"; do
 	python generate.py ${len}B_url.txt
 
-	for size in "${batch_size[@]}"; do
-		rm thp-*.txt latency-*.txt
+	for rate in $(seq 10 100 2000); do
 
-		dir=${len}B-full-batch=${size}-result
-		mkdir ${dir}
-		
-		for nr_core in $(seq 1 1 1); do 
-			echo ">> Full matching | Test with $nr_core cores >>"
+		for size in "${batch_size[@]}"; do
+			rm thp-*.txt latency-*.txt
 
-			mkdir ${dir}/thp-$nr_core/
-			mkdir ${dir}/lat-$nr_core/
+			dir=${len}B-full-batch=${size}-result
+			mkdir ${dir}
+			
+			for nr_core in $(seq 1 1 4); do 
+				echo ">> Full matching | Test with $nr_core cores >>"
 
-			for rate in $(seq 10 100 2000); do
+				mkdir ${dir}/thp-$nr_core/
+				mkdir ${dir}/lat-$nr_core/
+
 				rm thp-*.txt latency-*.txt
 				echo "  >> Full matching | Test $len B (round $round)"
+				# ./build/regex -l 50 -p 03:00.0 -r /tmp/full_url_regex_rules.rof2.binary -d $(pwd)/input.txt -c $nr_core -s $rate -q ${queue_depth} -a 1
 				./build/regex -l 50 -p 03:00.0 -r /tmp/full_url_regex_rules.rof2.binary -d $(pwd)/input.txt -c $nr_core -s $rate -q ${queue_depth} -a ${size}
-				cat thp-*.txt 		> ${dir}/thp-$nr_core/thp-rate-$rate.txt
-				cat latency-*.txt 	> ${dir}/lat-$nr_core/lat-rate-$rate.txt
+				mov thp-*.txt		${dir}/thp-$nr_core/
+				mov latency-*.txt 	${dir}/lat-$nr_core/
 				echo "  >> Test done!"
 				sleep 2
 			done
