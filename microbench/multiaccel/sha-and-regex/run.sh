f8cd48e8725417ac2978f657e584b7ace3840002
yihan <yangyihan1998@gmail.com>
2023-06-28 12:04:41 +0000

    updaye

--- microbench/multiaccel/sha-and-regex/run.sh
+++ microbench/multiaccel/sha-and-regex/run.sh
@@ -2,20 +2,18 @@
 data_size=(2048)
 
 for size in "${data_size[@]}"; do
-	dir=SHA=${size}B-result/
+	dir=SHA-${size}B-result/
 	mkdir ${dir}
 
-	for nr_core in $(seq 1 1 1); do 
-		echo ">> Test with $nr_core cores >>"
-
+	for rate in $(seq 100 100 3000); do
 		mkdir ${dir}/rate-$rate/
 		mkdir ${dir}/rate-$rate/
 
-		for rate in $(seq 100 100 3000); do
+		for nr_core in $(seq 1 1 1); do 
 			mkdir ${dir}/rate-$rate/thp-$nr_core/
 			mkdir ${dir}/rate-$rate/lat-$nr_core/
 
-			echo "  >> Test input $rate (Kops)"
+			echo "  >> Test input $rate (Kops) with $nr_core..."
 			./build/multiaccel -l 50 -p 03:00.0 -r /tmp/full_url_regex_rules.rof2.binary -f workload/workloada.spec -c $nr_core -s $rate -b $size
 			mv *-thp-*.txt 		${dir}/rate-$rate/thp-$nr_core/
 			mv latency-*.txt 	${dir}/rate-$rate/lat-$nr_core/
