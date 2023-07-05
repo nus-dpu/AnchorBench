a82e544b99048a08e034815e3a3f1a3f1f0e29aa
yihan <yangyihan1998@gmail.com>
2023-07-05 07:04:02 +0000

    update run.sh

--- dns-filter-accel-close/run.sh
+++ dns-filter-accel-close/run.sh
@@ -9,9 +9,10 @@ echo ">> Test with $nr_core cores >>"
 
 for rate in $(seq $start $step $max_rate); do
     echo ">> Rate at $rate... >>"
-    rm thp-*.txt
-    ./build/dns-filter -l 0-${nr_core} -n 4 -a 03:00.0 -a 03:00.1 -- -m "[1-$nr_core:-].0,[-:1-$nr_core].1" -- -l 50 -r /tmp/dns_baseline.rof2.binary -p 03:00.0
-    cat thp-*.txt > thp-$nr_core/thp-rate-$rate.txt
+    mkdir thp-$nr_core/rate-${rate}/
+    ./build/dns-filter -l 0-${nr_core} -n 4 -a 03:00.0 -a 03:00.1 -- -m "[1-$nr_core:-].0,[-:1-$nr_core].1" -q 128 -- -l 50 -r /tmp/dns_baseline.rof2.binary -p 03:00.0
+    mv thp-*.txt            thp-$nr_core/rate-${rate}/
+    mv network-thp-*.txt    thp-$nr_core/rate-${rate}/
     echo "  >> Test done!"
     sleep 2
 done
