b154f482ef5e1f6900f9d9b281a8e29667a96315
yihan <yangyihan1998@gmail.com>
2023-07-05 04:59:23 +0000

    update dns run script

--- dns-filter-accel/run_all.sh
+++ dns-filter-accel/run_all.sh
@@ -1,4 +1,8 @@
+max_rate=10000
 for core in $(seq 1 1 7); do
     rate=$((core * 2000))
+    if [ "$rate" -gt "${max_rate}" ]; then
+        rate=${max_rate}
+    fi
     bash run.sh $core 10 100 $rate
 done
