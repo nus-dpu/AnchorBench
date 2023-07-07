59673b80fb47e4462b407175421dff42315b7d17
yihan <yangyihan1998@gmail.com>
2023-07-07 09:04:57 +0000

    update sha

--- microbench/sha/run.sh
+++ microbench/sha/run.sh
@@ -1,27 +1,27 @@
 data_size=(16 32 64 128 256)
 per_core_rate=2500
+nr_core=8
 
 for size in "${data_size[@]}"; do
 	dir=${size}B-result/
 	mkdir ${dir}
 
-	for nr_core in $(seq 1 1 8); do 
-		echo ">> Test with $nr_core cores >>"
+	echo ">> Test with $nr_core cores >>"
 
-		max_rate=$((nr_core * per_core_rate))
+	max_rate=$((nr_core * per_core_rate))
 
-		mkdir ${dir}/thp-$nr_core/
-		mkdir ${dir}/lat-$nr_core/
+	mkdir ${dir}/thp-$nr_core/
+	mkdir ${dir}/lat-$nr_core/
 
-		for rate in $(seq 10 100 ${max_rate}); do
-			mkdir ${dir}/thp-$nr_core/rate-$rate/
-			mkdir ${dir}/lat-$nr_core/rate-$rate/
-			echo "  >> Test input $rate (Kops)"
-			./build/sha -l 50 -p 03:00.0 -d $(pwd)/input.dat -c $nr_core -s $rate -b $size
-			mv thp-*.txt 		${dir}/thp-$nr_core/rate-$rate/
-			mv latency-*.txt 	${dir}/lat-$nr_core/rate-$rate/
-			echo "  >> Test done!"
-			sleep 2
-		done
+	for rate in $(seq 10 100 ${max_rate}); do
+		mkdir ${dir}/thp-$nr_core/rate-$rate/
+		mkdir ${dir}/lat-$nr_core/rate-$rate/
+		echo "  >> Test input $rate (Kops)"
+		./build/sha -l 50 -p 03:00.0 -d $(pwd)/input.dat -c $nr_core -s $rate -b $size
+		mv thp-*.txt 		${dir}/thp-$nr_core/rate-$rate/
+		mv latency-*.txt 	${dir}/lat-$nr_core/rate-$rate/
+		echo "  >> Test done!"
+		sleep 2
 	done
+
 done
