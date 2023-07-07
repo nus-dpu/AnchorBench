4cf876ba24947311ed592083f6aa45a7054dca73
yihan <yangyihan1998@gmail.com>
2023-07-07 06:24:33 +0000

    update regex bash shell (16 worker, batch/no batch)

--- microbench/regex-2/run_dns.sh
+++ microbench/regex-2/run_dns.sh
@@ -1,7 +1,8 @@
 # queue_depth=(1 4 16 64 256)
 data_size=(20)
-batch_size=32
+batch_size=inf
 queue_depth=128
+nr_core=8
 
 for len in "${data_size[@]}"; do
 	python generate.py ${len}B_url.txt
@@ -12,21 +13,19 @@ for len in "${data_size[@]}"; do
 		dir=${len}B-full-batch=${size}-result
 		mkdir ${dir}
 		
-		for nr_core in $(seq 1 1 8); do
-            mkdir ${dir}/thp-$nr_core/
-			mkdir ${dir}/lat-$nr_core/
+		mkdir ${dir}/thp-$nr_core/
+		mkdir ${dir}/lat-$nr_core/
 
-			for rate in $(seq 10 100 5000); do
-                mkdir ${dir}/thp-$nr_core/rate-$rate/
-			    mkdir ${dir}/lat-$nr_core/rate-$rate/
+		for rate in $(seq 10 100 5000); do
+			mkdir ${dir}/thp-$nr_core/rate-$rate/
+			mkdir ${dir}/lat-$nr_core/rate-$rate/
 
-                echo "  >> Test input $rate (Kops) with data size $size Bytes"
-    			./build/regex -l 50 -p 03:00.0 -r /tmp/full_url_regex_rules.rof2.binary -d $(pwd)/input.txt -c $nr_core -s $rate -q ${queue_depth} -a ${size}
-                mv thp-*.txt		${dir}/thp-$nr_core/rate-$rate/
-                mv latency-*.txt 	${dir}/lat-$nr_core/rate-$rate/
-                echo "  >> Test done!"
-                sleep 2
-            done
+			echo "  >> Test input $rate (Kops) with data size $size Bytes"
+			./build/regex -l 50 -p 03:00.0 -r /tmp/full_url_regex_rules.rof2.binary -d $(pwd)/input.txt -c $nr_core -s $rate -q ${queue_depth} -a 16
+			mv thp-*.txt		${dir}/thp-$nr_core/rate-$rate/
+			mv latency-*.txt 	${dir}/lat-$nr_core/rate-$rate/
+			echo "  >> Test done!"
+			sleep 2
 		done
 	done
 done
