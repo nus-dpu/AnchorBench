95ddf5f18ef811af1c3a5e8ffa3bd7d9670e00c0
yihan <yangyihan1998@gmail.com>
2023-07-08 03:13:21 +0000

    update compress bash

--- microbench/compress/run.sh
+++ microbench/compress/run.sh
@@ -1,13 +1,14 @@
 data_size=(16 32 64 128 256 512 1024)
 
 for size in "${data_size[@]}"; do
-	mkdir ${size}B-result/
+	dir=${size}B-result/
+	mkdir ${dir}
 
 	for nr_core in $(seq 1 1 8); do 
 		echo ">> Test with $nr_core cores >>"
 
-		mkdir ${size}B-result/thp-$nr_core/
-		mkdir ${size}B-result/lat-$nr_core/
+		mkdir ${dir}/thp-$nr_core/
+		mkdir ${dir}/lat-$nr_core/
 
 		for rate in $(seq 10 100 4500); do
 			mkdir ${dir}/thp-$nr_core/rate-$rate/
