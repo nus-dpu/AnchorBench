ed692befdcbffb9b424bcee741346fecb62d7e6e
yihan <yangyihan1998@gmail.com>
2023-07-19 07:27:22 +0000

    update testpmd

--- testpmd/run_fwd.sh
+++ testpmd/run_fwd.sh
@@ -2,7 +2,7 @@ nr_core=(1 2 3 4 5 6 7)
 for core in "${nr_core[@]}"; do
     mkdir thp-$core/
 
-    echo ">> Test with $nr_core cores >>"
+    echo ">> Test with $core cores >>"
 
     rm thp-*.txt
     ./build/testpmd -l 0-$core -n 4 -a 03:00.0 -- -m "[1-$core:1-$core].0" -d 0
