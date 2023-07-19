f50c485203dda980571de369edbe624a5dff3529
yihan <yangyihan1998@gmail.com>
2023-07-19 06:04:44 +0000

    update testpmd

--- testpmd/testpmd-constants.h
+++ testpmd/testpmd-constants.h
@@ -6,12 +6,13 @@
 
 enum {
 	DEFAULT_PKT_BURST       = 32,	/* Increasing this number consumes memory very fast */
-	DEFAULT_RX_DESC         = 256,
+	DEFAULT_RX_DESC         = 1024,
 	DEFAULT_TX_DESC         = 2048,
 
 	MAX_MBUFS_PER_PORT      = (DEFAULT_TX_DESC * 8),/* number of buffers to support per port */
 	MAX_SPECIAL_MBUFS       = 64,
-	MBUF_CACHE_SIZE         = (MAX_MBUFS_PER_PORT / 8),
+	// MBUF_CACHE_SIZE         = (MAX_MBUFS_PER_PORT / 8),
+	MBUF_CACHE_SIZE         = 128,
 
 	DEFAULT_PRIV_SIZE       = 0,
 	MBUF_SIZE		= RTE_MBUF_DEFAULT_BUF_SIZE + DEFAULT_PRIV_SIZE, /* See: http://dpdk.org/dev/patchwork/patch/4479/ */
